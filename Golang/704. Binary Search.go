07130ef373e9fedc1f2764e3edb74955ec8f920a
Youra Cho <choyoura@gmail.com>
2022-03-23 22:05:02 -0700

    Add Golang/704. Binary Search.go file

---
+++ Golang/704. Binary Search.go
@@ -0,0 +1,16 @@
+// O(log n) / O(1)
+func search(nums []int, target int) int {
+    low, high := 0, len(nums)-1
+    
+    for low <= high {
+        mid :=  low + (high-low) // 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            high = mid-1
+        } else {
+            low = mid+1
+        }
+    }
+    return -1
+}
