96511f9549f0014e6a2bf9ef679b3bc6824063ae
espresso98 <espresso98@users.noreply.github.com>
2022-03-31 22:35:45 -0700

    Update 1929. Concatenation of Array.go

--- Golang/1929. Concatenation of Array.go
+++ Golang/1929. Concatenation of Array.go
@@ -1,13 +1,13 @@
-# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
-# Specifically, ans is the concatenation of two nums arrays. Return the array ans.
-# TC: O(n), SC: O(1)
+//  Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+//  Specifically, ans is the concatenation of two nums arrays. Return the array ans.
+// TC: O(n), SC: O(1)
 
-# Solution1
+//  Solution1
 func getConcatenation(nums []int) []int {
      return append(nums, nums...)
 }
 
-# Solution2
+//  Solution2
 func getConcatenation(nums []int) []int {
      n := len(nums)
      ans := make([]int, n*2)
@@ -18,7 +18,7 @@ func getConcatenation(nums []int) []int {
      return ans
 }
 
-# Solution3
+//  Solution3
 func getConcatenation(nums []int) []int {
     n := len(nums)
     res := make([]int, n*2)
