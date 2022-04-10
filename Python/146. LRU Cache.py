8bc11de875b19df32b434c69f62e3cec868d0452
Youra Cho <choyoura@gmail.com>
2022-04-09 23:52:33 -0700

    Add Python/146. LRU Cache.py file

---
+++ Python/146. LRU Cache.py
@@ -0,0 +1,53 @@
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+# Ordered dictionary
+# TC: O(1) for get/put, SC:O(capacity)
+
+from collections import OrderedDict
+
+class LRUCache:
+    # LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+    def __init__(self, capacity: int):
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+        
+    # int get(int key) Return the value of the key if the key exists, otherwise return -1.
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+    
+    # void put(int key, int value) Update the value of the key if the key exists. 
+    # Otherwise, add the key-value pair to the cache. 
+    # If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+            
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+# Input
+# ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+# [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+# Output
+# [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+# Explanation
+# LRUCache lRUCache = new LRUCache(2);
+# lRUCache.put(1, 1); // cache is {1=1}
+# lRUCache.put(2, 2); // cache is {1=1, 2=2}
+# lRUCache.get(1);    // return 1
+# lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+# lRUCache.get(2);    // returns -1 (not found)
+# lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+# lRUCache.get(1);    // return -1 (not found)
+# lRUCache.get(3);    // return 3
+# lRUCache.get(4);    // return 4
