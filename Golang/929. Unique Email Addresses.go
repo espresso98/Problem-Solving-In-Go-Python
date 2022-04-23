302779ecdf0944283e9cdd99c8479833fc3d20a6
Youra Cho <choyoura@gmail.com>
2022-04-23 07:31:31 -0700

    Correct the file

--- Golang/929. Unique Email Addresses.go
+++ Golang/929. Unique Email Addresses.go
@@ -1,13 +1,16 @@
 // Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
 // TC: O(n) / SC: O(n)
 
-class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:      
-        result = set()    
-        for email in emails:
-            local, domain = email.split('@')
-            local = local.split('+')[0]
-            local = local.replace('.', '')
-            result.add(local + '@' + domain)
-        return len(result)
-        
+func numUniqueEmails(emails []string) int {
+    set := make(map[string]bool)
+    
+    for _, email := range emails {
+        parts := strings.Split(email, "@")
+        // fmt.Println(parts)
+        local := strings.Split(parts[0], "+")
+        check := strings.Replace(local[0], ".", "", -1) + "@" + parts[1]
+        set[check] = true
+    }
+    
+    return len(set)
+}
