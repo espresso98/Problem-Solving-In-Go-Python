feeb697cc4d6b8121645b5bcd5bc076cb3fda679
Youra Cho <choyoura@gmail.com>
2022-06-09 20:22:09 -0700

    Add MergeSort Concurrent.go

---
+++ Golang/MergeSort Concurrent.go
@@ -0,0 +1,47 @@
+//  input: [9 4 3 6 1 2 10 5 7 8]
+// output: [1 2 3 4 5 6 7 8 9 10]
+
+package main
+
+import "fmt"
+
+func Merge(left, right []int) []int {
+	merged := make([]int, 0, len(left)+len(right))
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(merged, right...)
+		} else if len(right) == 0 {
+			return append(merged, left...)
+		} else if left[0] < right[0] {
+			merged = append(merged, left[0])
+			left = left[1:]
+		} else {
+			merged = append(merged, right[0])
+			right = right[1:]
+		}
+	}
+	return merged
+}
+
+func MergeSort(data []int) []int {
+	if len(data) <= 1 {
+		return data
+	}
+	done := make(chan bool) // new
+	mid := len(data) / 2
+	var left []int // new
+
+	go func() { // new
+		left = MergeSort(data[:mid])
+		done <- true // new
+	}() // new
+	right := MergeSort(data[mid:])
+	<-done // new
+	return Merge(left, right)
+
+}
+
+func main() {
+	data := []int{9, 4, 3, 6, 1, 2, 10, 5, 7, 8}
+	fmt.Printf("%v\n%v\n", data, MergeSort(data))
+}
