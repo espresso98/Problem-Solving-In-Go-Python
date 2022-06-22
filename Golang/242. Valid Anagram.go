915b0d892e3cb86c6178a99310de803876d3c015
Youra Cho <choyoura@gmail.com>
2022-06-21 18:26:56 -0700

    Update 242. Valid Anagram.go

--- Golang/242. Valid Anagram.go
+++ Golang/242. Valid Anagram.go
@@ -1,3 +1,4 @@
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 // TC: O(n), SC: O(1)
 
 func isAnagram(s string, t string) bool {
@@ -23,3 +24,18 @@ func isAnagram(s string, t string) bool {
 // Input: s = "anagram", t = "nagaram"
 // Output: true
 // "anagram" -> [3 0 0 0 0 0 1 0 0 0 0 0 1 1 0 0 0 1 0 0 0 0 0 0 0 0]
+
+// TC: O(nlogn), SC: O(1)
+// import (
+// 	"sort"
+// 	"strings"
+// )
+
+// func isAnagram(s string, t string) bool {
+// if len(s) != len(t) { return false }
+// a := strings.Split(s, "")
+// b := strings.Split(t, "")
+// sort.Strings(a)
+// sort.Strings(b)
+// return strings.Join(a,"") == strings.Join(b,"")
+// }
