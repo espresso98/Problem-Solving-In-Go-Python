aa70d5b71f5dbd4d311d699a3d20724a2590f965
Youra Cho <choyoura@gmail.com>
2022-06-21 18:41:11 -0700

    Update 49. Group Anagrams.go

--- Golang/49. Group Anagrams.go
+++ Golang/49. Group Anagrams.go
@@ -4,11 +4,11 @@ func groupAnagrams(strs []string) [][]string {
 	var res [][]string
 	dict := make(map[[26]int][]string)
 	for _, s := range strs {
-		var count [26]int
+		var count [26]int // key
 		for _, ch := range s {
 			count[ch-'a']++
 		}
-		dict[count] = append(dict[count], s)
+		dict[count] = append(dict[count], s) // value
 		fmt.Println(count)
 	}
 
@@ -22,12 +22,8 @@ func groupAnagrams(strs []string) [][]string {
 // Input: strs = ["eat","tea","tan","ate","nat","bat"]
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-/*
-[1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]  <- "eat"
-[1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]  <- "tea"
-[1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0]  <- "tan"
-[1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]  <- "ate"
-[1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0]  <- "nat"
-[1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]  <- "bat"
-[[bat] [eat tea ate] [tan nat]]
+/* key : value
+map[[1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0]:[tan nat]
+    [1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]:[eat tea ate]
+	[1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]:[bat]]
 */ 
