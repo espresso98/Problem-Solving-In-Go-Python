7467f16d1f134e4a42029551dac298438131419d
Youra Cho <choyoura@gmail.com>
2022-11-17 10:46:59 -0800

    Add 1523. Count Odd Numbers in an Interval Range.py

---
+++ Python/1523. Count Odd Numbers in an Interval Range.py
@@ -0,0 +1,12 @@
+# TC: O(1), SC: O(1)
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        # Soultion 1
+        range = high + 1 - low
+        if low % 2 == 1 and high % 2 == 1:
+            return range // 2 + 1
+        else:
+            return range // 2
+
+        # Solution 2
+        return (high + 1) // 2 - low//2   
