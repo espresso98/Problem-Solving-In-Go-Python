0445e6fbec07fbe5aeb5904353d2b5eba56bc240
Youra Cho <choyoura@gmail.com>
2022-12-11 15:57:52 -0800

    Add 124. Binary Tree Maximum Path Sum.py

---
+++ Python/124. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,16 @@
+# max_path_sum = max(max_path_sum, left_gain + right_gain + root.val)
+#  O(n, )O(n)
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        def tree_path_sum(node):
+            if not node: return 0
+            left = max(tree_path_sum(node.left), 0)
+            right = max(tree_path_sum(node.right), 0)
+            cur_max_sum = left + right + node.val
+            self.max_path_sum = max(self.max_path_sum, cur_max_sum)
+            # print(left, right, node.val, cur_max_sum, self.max_path_sum)
+            return max(left, right) + node.val
+
+        self.max_path_sum = -math.inf
+        tree_path_sum(root)
+        return self.max_path_sum 
