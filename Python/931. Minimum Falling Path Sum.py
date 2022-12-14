b86af173024b00221b5bb29b0ccc8436d27f38f5
Youra Cho <choyoura@gmail.com>
2022-12-14 10:52:46 -0800

    Add 931. Minimum Falling Path Sum.py

---
+++ Python/931. Minimum Falling Path Sum.py
@@ -0,0 +1,13 @@
+# O(N^2), O(N)
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n, M = len(M), matrix
+        dp = [float('inf')] + M[0] + [float('inf')]
+
+        for i in range(1, n):
+            cur_row = [float('inf')] + M[i] + [float('inf')]
+            for j in range(1, n + 1):
+                cur_row[j] = cur_row[j] + min(dp[j-1], dp[j], dp[j+1])
+            dp = cur_row   # min_path_sum_so_far
+
+        return min(dp)
