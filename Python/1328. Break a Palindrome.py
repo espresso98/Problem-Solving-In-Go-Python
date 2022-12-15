686de55409f76db59250a42f9e313233fe7d6e64
Youra Cho <choyoura@gmail.com>
2022-12-15 11:45:11 -0800

    Add 1328. Break a Palindrome.py

---
+++ Python/1328. Break a Palindrome.py
@@ -0,0 +1,9 @@
+# O(N), O(1)
+ def breakPalindrome(self, palindrome: str) -> str:
+        n = len(palindrome)
+        if n < 2: return ''
+        for i in range(n//2):
+            if palindrome[i] != 'a':
+                return palindrome[:i] + 'a' + palindrome[i+1:]
+        return palindrome[:-1] + 'b'
+        
