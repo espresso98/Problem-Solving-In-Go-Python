7d4f8aa10ec4a14c56f3aeca69d1bac5de7eaee5
Youra Cho <choyoura@gmail.com>
2023-01-03 21:38:43 -0800

    Add 2043. Simple Bank System_v2.py

--- Python/2043. Simple Bank System_v2.py
+++ Python/2043. Simple Bank System_v2.py
@@ -1,4 +1,4 @@
-# TC: O(1), SC: O(1)
+
 class Bank:
 
     def __init__(self, balance: List[int]):
@@ -14,8 +14,6 @@ class Bank:
                 \nCurrent balance of the account{account1} is ${self.balance[account1-1]}.\
                 \nCurrent balance of the account{account2} is ${self.balance[account2-1]}.")
                 return True
-            self.print_line()
-            print("Low balance!")
             return False
         return False
 
@@ -23,7 +21,7 @@ class Bank:
         if self.valid_account(account):
             self.balance[account-1] += money
             self.print_line()
-            print(f"Deposited ${money} to account{account}! \nCurrent balance is ${self.balance[account-1]}.")
+            print(f"Deposited ${money} to account{account}! \nCurrent balance account{account} is ${self.balance[account-1]}.")
             return True
         return False
 
@@ -32,18 +30,28 @@ class Bank:
             if self.valid_balance(account, money):
                 self.balance[account-1] -= money
                 self.print_line()
-                print(f"Withdrew ${money} from account{account}! \nCurrent balance is ${self.balance[account-1]}.")
+                print(f"Withdrew ${money} from account{account}! \nCurrent balance of account{account} is ${self.balance[account-1]}.")
                 return True
-            self.print_line()
-            print("Low balance!")
             return False
         return False
     
     def valid_account(self, account: int) -> bool:
-        return 1 <= account <= len(self.balance)
+        if 1 <= account <= len(self.balance):
+            return True
+        else:
+            self.print_line()
+            print("Invalid account!")
+            return False
+
 
     def valid_balance(self, account: int, money: int) -> bool:
-        return self.balance[account-1] >= money
+        if self.balance[account-1] >= money:
+            return True
+        else:
+            self.print_line()
+            print("Low balance!")
+            return False
+
 
     def print_line(self) -> str:
         print("--------------------------------------------")
@@ -55,25 +63,18 @@ class Bank:
 # param_2 = obj.deposit(account,money)
 # param_3 = obj.withdraw(account,money)
 
-"""
-Input
-["Bank", "withdraw", "transfer", "deposit", "transfer", "withdraw"]
-[[[10, 100, 20, 50, 30]], [3, 10], [5, 1, 20], [5, 20], [3, 4, 15], [10, 50]]
 
-Output
-[null, true, true, true, false, false]
-
-Stdout
---------------------------------------------
-Withdrew $10 from account3! 
-Current balance is $10.
---------------------------------------------
-Transferred $20 from account5 to account1!                 
-Current balance of the account5 is $10.                
-Current balance of the account1 is $30.
---------------------------------------------
-Deposited $20 to account5! 
-Current balance is $30.
---------------------------------------------
-Low balance!
-"""
+# --------------------------------------------
+# Withdrew $10 from account3! 
+# Current balance of account3 is $10.
+# --------------------------------------------
+# Transferred $20 from account5 to account1!                 
+# Current balance of the account5 is $10.                
+# Current balance of the account1 is $30.
+# --------------------------------------------
+# Deposited $20 to account5! 
+# Current balance account5 is $30.
+# --------------------------------------------
+# Low balance!
+# --------------------------------------------
+# Invalid account!
