38bb7d5dd846693943851cef2cc728a9c269d776
Youra Cho <choyoura@gmail.com>
2023-01-11 13:39:19 -0800

    Add an iterative solution of 100. Same Tree.py

--- Python/100. Same Tree.py
+++ Python/100. Same Tree.py
@@ -1,14 +1,36 @@
 # Definition for a binary tree node.
+from typing import Optional
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
         
-# O(N) in time, O(N) in space
+# O(N), O(N)
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q: return True
         elif not p or not q: return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
+# Iteration: O(n) / O(n) 
+class Solution2:
+    from collections import deque
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def isSame(node1, node2):
+            if not node1 and not node2: return True
+            if not node1 or not node2: return False
+            if node1.val != node2.val: return False
+            return True
+
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if not isSame(p, q):
+                return False
+            if p:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+
+        return True
