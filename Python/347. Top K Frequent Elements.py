c66e1c64fa6ab460ae67a4c344d7f42d1d065287
Youra Cho <choyoura@gmail.com>
2023-01-13 19:15:39 -0800

    Add Bucket sort approach: O(N)

--- Python/347. Top K Frequent Elements.py
+++ Python/347. Top K Frequent Elements.py
@@ -8,3 +8,21 @@ class Solution:
         heap = [(-freq, num) for num, freq in num_cnts.items()]
         heapify(heap)  # [(-3, 1), (-2, 2), (-1, 3)]
         return [heappop(heap)[1] for _ in range(k)]
+
+
+# Bucket sort: O(N), O(N)
+class Solution2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count_nums = collections.Counter(nums)      # {1: 3, 2: 2, 3: 1}
+        freqs = [[] for i in range(len(nums) + 1)]
+        
+        for num, cnt in count_nums.items():       # freq    0    1    2    3    4   5   6 
+            freqs[cnt].append(num)                # bucket [[], [3], [2], [1], [], [], []]
+
+        res = []
+        for freq in range(len(freqs) - 1, 0, -1 ):
+            for n in freqs[freq]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+            
