a5f739d70f4208db19b8df3d3e52b36b106f2a34
Youra Cho <choyoura@gmail.com>
2023-01-16 15:21:55 -0800

    Update 2421. Number of Good Paths.py

--- Python/2421. Number of Good Paths.py
+++ Python/2421. Number of Good Paths.py
@@ -9,7 +9,7 @@ class Solution:
             adj[u].add(v)
             adj[v].add(u)
 
-        # find {val:nodes} mapping
+        # find {val:{nodes}} mapping
         same_val_nodes = collections.defaultdict(set)  # {1: {0, 3}, 3: {1, 4}, 2: {2}}
         for idx, val in enumerate(vals):
             same_val_nodes[val].add(idx)
@@ -27,23 +27,24 @@ class Solution:
         # union by rank, link tree w/smaller rank to tree w/larger rank  
         def union(x, y):  
             rootX, rootY = find(x), find(y)
-            if rank[rootX] >= rank[rootY]:
-                root[rootY] = rootX
-                rank[rootX] += rootY
-            else:
-                root[rootX] = rootY
-                rank[rootY] += rootX
+            if rootX != rootY: 
+                if rank[rootX] >= rank[rootY]:
+                    root[rootY] = rootX
+                    rank[rootX] += rootY
+                else:
+                    root[rootX] = rootY
+                    rank[rootY] += rootX
 
         # find the number of distinct good paths
         res = 0   
         for val in sorted(same_val_nodes.keys()):   # {1: {0, 3}, 2: {2}, 3: {1, 4}}
-            for i in same_val_nodes[val]:   # {0, 3}  {2}  {1, 4}
-                for j in adj[i]:            # {0: {1, 2}, 1: {0}, 2: {0, 3, 4}, 3: {2}, 4: {2}}
-                    if vals[j] <= val:      # Only choose neighbors with a smaller value or equal to the starting node
-                        union(i, j)         #         (2,0) (2,3)  1 <= 2   (1,0) 1 <= 3 (4,2) 2 <= 3 
+            for i in same_val_nodes[val]:    # {0, 3}  {2}  {1, 4}
+                for nb in adj[i]:            # {0: {1, 2}, 1: {0}, 2: {0, 3, 4}, 3: {2}, 4: {2}}
+                    if vals[nb] <= val:      # Only choose neighbors with a smaller value or equal to the starting node
+                        union(nb, i)        
 
             cnts = collections.Counter()
             for i in same_val_nodes[val]:  # {0, 3}  {2}  {1, 4}
-                cnts[find(i)] += 1         # {0: 1, 3: 1}, {2: 1}, {2: 2}
+                cnts[find(i)] += 1         # {0: 1} {0: 1, 3: 1}
                 res += cnts[find(i)]       # 1, 2, 3, 4, 6
         return res
