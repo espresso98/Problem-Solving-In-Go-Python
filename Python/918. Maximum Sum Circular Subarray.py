a0b999043be8d4f05e0c8a492f5792611010a121
Youra Cho <choyoura@gmail.com>
2023-01-19 15:46:26 -0800

    Update 918. Maximum Sum Circular Subarray.py

--- Python/918. Maximum Sum Circular Subarray.py
+++ Python/918. Maximum Sum Circular Subarray.py
@@ -22,3 +22,22 @@ class Solution:
          # If all numbers are negative sum(nums) = min_sum, circular_sum = 0
          # return max_sum = max(nums) < 0
         return max_sum if max_sum <= 0 else max(max_sum, circular_sum) 
+
+
+class Solution2:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        # If all numbers are negative return max(nums)
+        if all(num < 0 for num in nums): 
+            return max(nums)
+
+        cur_max = cur_min = max_sum = min_sum = nums[0]
+        for num in nums[1:]:
+            # noncircular_max_sum: Kadane's Algorithm(maximum_sum subarray)
+            cur_max = max(cur_max, 0) + num
+            max_sum = max(max_sum, cur_max)
+            # circular_max_sum: max(prefix + suffix) = total_sum - min(subarray)
+            cur_min = min(cur_min, 0) + num
+            min_sum = min(min_sum, cur_min)   
+
+        return max(max_sum, sum(nums) - min_sum) 
+        
