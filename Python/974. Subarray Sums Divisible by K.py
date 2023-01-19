d380e6379bc28ca72363da938b1a4a1925d3f6b6
Youra Cho <choyoura@gmail.com>
2023-01-19 15:46:44 -0800

    Update 974. Subarray Sums Divisible by K.py

--- Python/974. Subarray Sums Divisible by K.py
+++ Python/974. Subarray Sums Divisible by K.py
@@ -1,16 +1,33 @@
-# Prefix Sums and Counting: O(N+K), O(K)
+# Prefix Sums and Counting: O(N), O(K)
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         res = rsum = 0
-        prefix = {0:1}    
+        remainders = {0:1}    
         
         for num in nums:  
             rsum += num  
             remain = rsum % k
-            if remain in prefix:
-                res += prefix[remain]
-                prefix[remain] += 1
+            if remain in remainders:
+                res += remainders[remain]
+                remainders[remain] += 1
             else: 
-                prefix[remain] = 1
+                remainders[remain] = 1
         return res
-        
+
+# O(N), O(K)  Prefix sum + Hashmap  
+# sum(i, j) = sum(0, j) - sum(0, i) 
+# if sum(0, i) % K == sum(0, j) % K, sum(i, j) is divisible by K 
+import collections
+class Solution2:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:       
+        res = prefix_sum = 0
+        remainders = collections.defaultdict(int)
+        remainders[0] = 1
+
+        for num in nums:
+            prefix_sum += num
+            rem  = prefix_sum % k
+            if rem in remainders:       
+                res += remainders[rem]  
+            remainders[rem] += 1
+        return res 
