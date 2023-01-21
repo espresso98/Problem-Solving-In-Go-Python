9c02cb5f73f6618510eeebc68823d5ef57cc2bae
Youra Cho <choyoura@gmail.com>
2023-01-20 20:14:05 -0800

    Update 93. Restore IP Addresses.py

--- Python/93. Restore IP Addresses.py
+++ Python/93. Restore IP Addresses.py
@@ -1,28 +1,41 @@
 # valid : exactly four integers separated by single dots
 # each integer : between 0 and 255
 #              : cannot have leading zeros.
-# decision tree. 3 decisions. 3^12. len <= 12
 # Approach: Backtracking (DFS)
-
+# TC: O(M^N*N) where O(M^N−1) possibilities * O(M⋅N) valid check, here M=3, N=4
+# SC: O(M*N)
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         res = []
-        if len(s) > 12:
+        N = len(s)
+
+        if N < 4 or N > 12:
             return res
         
-        def backtrack(i, dots, curIP):
-            if dots == 4 and i == len(s):
-                res.append(curIP[:-1])
+        def backtrack(i, dots, current):
+            if dots < 0: 
                 return
-            if dots > 4:
-                return 
+
+            if i == N:
+                if dots == 0:
+                    res.append(".".join(current))
+                return
+             
+            if s[i] == "0":
+                current.append("0")
+                backtrack(i+1, dots-1, current)
+                current.pop()
+                return
             
-            for j in range(i, min(i+3, len(s))):
-                if int(s[i:j+1]) < 256 and (len(s[i:j+1]) == 1 or s[i] != "0"):
-                    backtrack(j + 1, dots + 1, curIP + s[i:j+1] + "." )
-                     
-        backtrack(0, 0, "")
+            for j in range(3):
+                if i + j < N and  0 <= int(s[i:i+j+1]) <= 255:
+                    current.append(s[i:i+j+1])
+                    backtrack(i+j+1, dots-1, current)
+                    current.pop()
+                    
+        backtrack(0, 4, [])
         return res
-    
+        
+
 # Input: s = "101023"
 # Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
