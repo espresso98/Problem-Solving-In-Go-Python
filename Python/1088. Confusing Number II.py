78e61d8a3086ec7f38d6f974d0049cf8727862c9
Youra Cho <choyoura@gmail.com>
2023-01-29 16:49:20 -0800

    Add 860. Lemonade Change.py Solution2

--- Python/1088. Confusing Number II.py
+++ Python/1088. Confusing Number II.py
@@ -1,4 +1,21 @@
-class Solution:
+class Solution1:
+    def confusingNumberII(self, n: int) -> int:
+        rotation = {0:0, 1:1, 6:9, 8:8, 9:6}
+        self.res = 0
+
+        def dfs(num, numRotated, base):
+            if num != numRotated:
+                self.res += 1
+            for d, dRotated in rotation.items():
+                if d == 0 and num == 0: continue  
+                if num * 10 + d > n: break  
+                dfs(num * 10 + d, dRotated * base + numRotated, base * 10)
+                # (1 1 10) (10 1 100) (11 11 100) (16 91 100) (18 81 100) (19 61 100) (6 9 10) (8 8 10) (9 6 10)
+        dfs(0, 0, 1)
+        return self.res
+
+
+class Solution2:
     def confusingNumberII(self, n: int) -> int:
         def dfs(cur: int, rotation: int, base: int) -> int:
             res = 0 
